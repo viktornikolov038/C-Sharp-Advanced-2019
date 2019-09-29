e88747edfd33feffbc87dad3a566796a038e193f
Viktor Nikolov <35701093+viktornikolov038@users.noreply.github.com>
2019-09-29 13:42:06 +0300

    Add files via upload

---
+++ 04.Streams/Streams Demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams_Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var fileNameAndPath = "../../text.txt";
+            var encoding = Encoding.UTF8;
+            var append = true;
+            var stream = new StreamWriter(fileNameAndPath,append,encoding);
+            using (stream)
+            {
+                stream.AutoFlush = true;
+                stream.WriteLine("Next Line");
+            }
+            
+        }
+    }
+}
